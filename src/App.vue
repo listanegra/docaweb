7477ad6479539a222ca7b9d769d1f418e35b69f7
VitorMac10 <vitormac3@hotmail.com>
2020-09-07 16:19:16 -0300

    Estrutura do projeto

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <q-layout view="lHh Lpr lFf">
+        <q-header elevated class="glossy">
+            <q-toolbar>
+            </q-toolbar>
+        </q-header>
+
+        <q-page-container>
+
+        </q-page-container>
+    </q-layout>
+</template>
---
+++ src/quasar.ts
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import { Quasar } from 'quasar'
+
+import '@/styles/quasar.scss'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/mdi-v4/mdi-v4.css'
+
+const lang = require('quasar/lang/pt-br.js');
+const iconSet = require('quasar/icon-set/mdi-v4.js');
+
+Vue.use(Quasar, {
+    config: {},
+    plugins: {},
+    lang, iconSet
+});
