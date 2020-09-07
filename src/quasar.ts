4d410017c9143aa2969391e294be0e08e0aefeef
VitorMac10 <vitormac3@hotmail.com>
2020-09-07 18:20:04 -0300

    Ajuste ícones quasar

--- src/quasar.ts
+++ src/quasar.ts
@@ -5,11 +5,10 @@ import '@/styles/quasar.scss'
 import '@quasar/extras/roboto-font/roboto-font.css'
 import '@quasar/extras/mdi-v4/mdi-v4.css'
 
-const lang = require('quasar/lang/pt-br.js');
-const iconSet = require('quasar/icon-set/mdi-v4.js');
+import MDI from 'quasar/icon-set/mdi-v5'
 
 Vue.use(Quasar, {
     config: {},
-    plugins: [Loading, Notify, LocalStorage],
-    lang, iconSet
+    iconSet: MDI,
+    plugins: [Loading, Notify, LocalStorage]
 });
